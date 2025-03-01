d45b41a3af0c31e05e32a0922557ba1c8e21093f
Carston Wiebe <ctwiebe23@gmail.com>
2025-03-01 13:58:30 -0600

    got website framework set up

--- core.js
+++ core.js
@@ -1,125 +1,68 @@
-// All the paths
-var paths = [];
-// Are we painting?
-var painting = false;
-// How long until the next circle
-var next = 0;
-// Where are we now and where were we?
-var current;
-var previous;
+const canvasContainerId = 'canvas-container';
+const canvasContainer = document.getElementById(canvasContainerId);
 
+function getWindowDimensions() {
+    return {
+        width: document.body.clientWidth - 128 - 128,
+        height: document.body.clientHeight - 32
+    };
+}
+
+/* * * * * * * * * * * * * * * *
+ * p5.js built-in functions
+ * * * * * * * * * * * * * * * */
+
+/**
+ * Called once on start-up
+ */
 function setup() {
-  let canvas = createCanvas(720, 400);
-  canvas.parent('main');
-  current = createVector(0,0);
-  previous = createVector(0,0);
-};
+    let { width, height } = getWindowDimensions();
+    let canvas = createCanvas(width, height, WEBGL);
+    canvas.parent(canvasContainerId);
 
+    angleMode(RADIANS);
+
+    strokeWeight(5);
+    stroke(32, 8, 64);
+
+    noFill();
+}
+
+/**
+ * Called when window is resized
+ */
+function windowResized() {
+    let { width, height } = getWindowDimensions();
+    resizeCanvas(width, height);
+}
+
+/**
+ * Called periodically according to framerate
+ */
 function draw() {
-  background(200);
-  
-  // If it's time for a new point
-  if (millis() > next && painting) {
+    background(250, 180, 200);
+    frameRate(30);
 
-    // Grab mouse position      
-    current.x = mouseX;
-    current.y = mouseY;
+    orbitControl(2, 2, 2);  // Allow use to move camera
 
-    // New particle's force is based on mouse movement
-    var force = p5.Vector.sub(current, previous);
-    force.mult(0.05);
+    push();
+    box();
+    pop();
 
-    // Add new particle
-    paths[paths.length - 1].add(current, force);
-    
-    // Schedule next circle
-    next = millis() + random(100);
+    // // Rotate rings in a half circle to create a sphere of cubes
+    // for (let zAngle = 0; zAngle < 180; zAngle += 30) {
+    //   // Rotate cubes in a full circle to create a ring of cubes
+    //   for (let xAngle = 0; xAngle < 360; xAngle += 30) {
+    //     push();
 
-    // Store mouse values
-    previous.x = current.x;
-    previous.y = current.y;
-  }
+    //     // Rotate from center of sphere
+    //     rotateZ(zAngle);
+    //     rotateX(xAngle);
 
-  // Draw all paths
-  for( var i = 0; i < paths.length; i++) {
-    paths[i].update();
-    paths[i].display();
-  }
-}
-
-// Start it up
-function mousePressed() {
-  next = 0;
-  painting = true;
-  previous.x = mouseX;
-  previous.y = mouseY;
-  paths.push(new Path());
-}
-
-// Stop
-function mouseReleased() {
-  painting = false;
-}
-
-// A Path is a list of particles
-function Path() {
-  this.particles = [];
-  this.hue = random(100);
-}
-
-Path.prototype.add = function(position, force) {
-  // Add a new particle with a position, force, and hue
-  this.particles.push(new Particle(position, force, this.hue));
-}
-
-// Display plath
-Path.prototype.update = function() {  
-  for (var i = 0; i < this.particles.length; i++) {
-    this.particles[i].update();
-  }
-}  
-
-// Display plath
-Path.prototype.display = function() {
-  
-  // Loop through backwards
-  for (var i = this.particles.length - 1; i >= 0; i--) {
-    // If we shold remove it
-    if (this.particles[i].lifespan <= 0) {
-      this.particles.splice(i, 1);
-    // Otherwise, display it
-    } else {
-      this.particles[i].display(this.particles[i+1]);
-    }
-  }
-
-}  
-
-// Particles along the path
-function Particle(position, force, hue) {
-  this.position = createVector(position.x, position.y);
-  this.velocity = createVector(force.x, force.y);
-  this.drag = 0.95;
-  this.lifespan = 255;
-}
-
-Particle.prototype.update = function() {
-  // Move it
-  this.position.add(this.velocity);
-  // Slow it down
-  this.velocity.mult(this.drag);
-  // Fade it out
-  this.lifespan--;
-}
-
-// Draw particle and connect it with a line
-// Draw a line to another
-Particle.prototype.display = function(other) {
-  stroke(0, this.lifespan);
-  fill(0, this.lifespan/2);    
-  ellipse(this.position.x,this.position.y, 8, 8);    
-  // If we need to draw a line
-  if (other) {
-    line(this.position.x, this.position.y, other.position.x, other.position.y);
-  }
+    //     // Then translate down 400 units
+    //     translate(0, 400, 0);
+    //     box();
+    //     pop();
+    //   }
+    // }
 }
